08285a4cbcd5cfaf9dc59d74f604960f8e5da907
camposyuri <yurinapoleao@gmail.com>
2020-10-22 13:28:27 -0300

    [ADD] - Fix routes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -12,4 +12,24 @@ routes.get("/posts", async (request, response) => {
     });
 });
 
+routes.post("/posts", (request, response) => {
+  const post = { ...request.body };
+
+  try {
+    function notNull(value, msg) {
+      if (value === "" || value === null) {
+        throw msg;
+      }
+    }
+    notNull(post.message, "Message not informed.");
+
+    knex("posts")
+      .insert(post)
+      .then((_) => response.status(204).send())
+      .catch((err) => response.status(500).send(err));
+  } catch (msg) {
+    response.status(400).send(msg);
+  }
+});
+
 module.exports = routes;
