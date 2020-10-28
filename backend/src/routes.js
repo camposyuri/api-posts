ca47cc94bec223d139b3f7d080742adde1b2f050
camposyuri <yurinapoleao@gmail.com>
2020-10-28 15:33:47 -0300

    [ADD] - Create Route GET /posts/:id/comments

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,8 +3,13 @@ const routes = express.Router();
 
 const knex = require("./database");
 
-// Routes Posts
+function notNull(value, msg) {
+  if (value === "" || value === null) {
+    throw msg;
+  }
+}
 
+// Routes Posts
 routes.get("/posts", async (request, response) => {
   return knex("posts")
     .select("*")
@@ -18,12 +23,8 @@ routes.post("/posts", (request, response) => {
   const post = { ...request.body };
 
   try {
-    function notNull(value, msg) {
-      if (value === "" || value === null) {
-        throw msg;
-      }
-    }
     notNull(post.message, "Message not informed.");
+    console.log("here");
 
     knex("posts")
       .insert(post)
@@ -44,12 +45,46 @@ routes.get("/posts/:id", (request, response) => {
     .catch((err) => response.status(404).send(err));
 });
 
+routes.get("/posts/:id/comments", (request, response) => {
+  knex("comments")
+    .where({ postId: request.params.id })
+    .then((comment) => {
+      return response.json(comment);
+    })
+    .catch((err) => response.status(404).send(err));
+});
+
+routes.post("/posts/:id/comments", (request, response) => {
+  const comment = { ...request.body };
+
+  try {
+    notNull(comment.comment, "Comment cannot be empty");
+
+    knex("comments")
+      .insert(comment)
+      .then((_) => response.status(204).send())
+      .catch((err) => response.status(500).send(err));
+  } catch (msg) {
+    response.status(400).send(msg);
+  }
+});
+
 // Routes Comments
 routes.get("/comments", async (request, response) => {
   return knex("comments")
     .select("*")
     .orderBy("id")
     .then((comment) => response.json(comment))
+    .catch((err) => response.status(404).send(err));
+});
+
+routes.get("/comments/:id", (request, response) => {
+  knex("comments")
+    .where({ id: request.params.id })
+    .first()
+    .then((comment) => {
+      return response.json(comment);
+    })
     .catch((err) => response.status(404).send(err));
 });
 
